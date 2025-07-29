cd3664b738737c589a8dce334dd73c80164e0444
atharva k <athakhai2020@gmail.com>
2025-07-29 17:06:39 -0500

    backend setup, added auth controller and auth routes

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("Signup Route Called");
+}
+
+export const login = (req, res) => {
+    res.send("Login Route Called");
+}
+
+export const logout = (req, res) => {
+    res.send("Logout Route Called");
+}
