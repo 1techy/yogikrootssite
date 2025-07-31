0a470740189a211fbfe507ec8c06a05c040e940e
atharva k <athakhai2020@gmail.com>
2025-07-31 11:06:05 -0500

    adding backend api and authentication

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 //console.log(process.env.PORT);
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
